fa7f195c256241692d412360f048fa4d01ca5826
aktsiy <2000s0201@gmail.com>
2019-09-05 08:12:02 +0900

    分岐1

--- a_thx.php
+++ a_thx.php
@@ -56,7 +56,7 @@ $name = $_POST['name(f)'];
 	</div>
 	
 	<p class="center"><?php echo 'your name　　' . $name; ?></p>
-	
+	<div>1</div>
 	<div class="container">
 		<div class="container">
 			<div class="container">
