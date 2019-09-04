ad13c956b2c4d114ab45f2a204adc13566851689
aktsiy <2000s0201@gmail.com>
2019-09-05 06:25:24 +0900

    newタブのcreateから新しいツリーーを作れる。 ここでは復習のために作ったa_thx.phpで試していく。

---
+++ a_thx.php
@@ -0,0 +1,101 @@
+<?php 
+if ($_SERVER['REQUEST_METHOD']==='GET') {
+	header('Location: a_input.php');
+}
+$name = $_POST['name(f)'];
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="a_act.css">
+	<title>compeleted sending</title>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+		</div>
+		<div class="row">
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+		</div>
+		<div class="row">
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+		</div>
+		<div class="row">
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+		</div>
+		<div class="row">
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+		</div>
+		<div class="row">
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+		</div>
+
+	</div>
+	
+	<p class="center"><?php echo 'your name　　' . $name; ?></p>
+	<div class="container">
+		<div class="container">
+			<div class="container">
+		<div class="row">
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+		</div>
+		<div class="row">
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+		</div>
+		<div class="row">
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+		</div>
+		<div class="row">
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+		</div>
+		<div class="row">
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+		</div>
+		<div class="row">
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+			<p class="col-3">THANK YOU</p>
+		</div>
+		</div>
+	</div>
+</body>
+</html>
